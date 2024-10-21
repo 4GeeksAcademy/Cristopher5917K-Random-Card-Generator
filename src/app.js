aa146ba278fd28477bd0839914bce3176808eee8
Cristopher5917K <garcacristopher3@gmail.com>
2024-10-21 21:37:16 +0000

    New changes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,24 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numCard = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let picas = [`♦`, `♥`, `♠`, `♣`];
+  let picasRandom = Math.floor(Math.random() * picas.length);
+
+  let card = document.querySelector("#card");
+  let divTop = document.createElement("div");
+  let divMid = document.createElement("div");
+  let divBotoom = document.createElement("div");
+
+  card.classList.add("card");
+  card.appendChild(divTop).classList = "picas";
+  card.appendChild(divMid).classList = "numCard";
+  card.appendChild(divBotoom).classList = "picasBottom";
+
+  divTop.textContent = picas[picasRandom];
+  divMid.textContent = Math.floor(Math.random() * numCard.length);
+  divBotoom.textContent = picas[picasRandom];
 };
